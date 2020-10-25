90c07fc0396d9edb6c0b57711cb437322bda6d65
Mauricio Puccinelli <puccinellimauricio@gmail.com>
2020-10-25 16:53:53 -0300

    Excercise N°1

---
+++ index.js
@@ -0,0 +1,15 @@
+/* 4.a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la
+suma de ambos números en una 3er variable. */
+
+var num1 = 10;
+var num2 = 15;
+var add = num1+num2;
+console.log(add);
+
+/* 4.b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er
+variable. */
+
+var string1 = 'Hello';
+var string2 = ' World';
+var hw = string1+string2;
+console.log(hw);
