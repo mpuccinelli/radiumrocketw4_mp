ba3d16a4b2315be980d40f06b1321bf0baadd78d
Mauricio Puccinelli <puccinellimauricio@gmail.com>
2020-10-25 18:44:33 -0300

    Excercise N°4

--- index.js
+++ index.js
@@ -77,44 +77,83 @@ consola los meses 5 y 11 (utilizar console.log) */
 
 var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 var months2 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-console.log(months[4],months[10]);
+//console.log(months[4],months[10]);
 
 /* 3.b) Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort). */
 
 var months = months.sort();
-console.log(months);
+//console.log(months);
 
 /* 3.c) Agregar un elemento al principio y al final del array (utilizar unshift y push) */
 
 months.unshift('First Month');
 months.push('Ending Month');
-console.log(months);
+//console.log(months);
 
 /* 3.d) Quitar un elemento del principio y del final del array (utilizar shift y pop) */
 
 months.shift();
 months.pop();
-console.log(months);
+//console.log(months);
 
 /* 3.e) Invertir el orden del array (utilizar reverse)*/
 
 months.reverse();
-console.log(months);
+//console.log(months);
 
 
 /* 3.f) Unir todos los elementos del array en un único string donde cada mes este separado
 por un guión - (utilizar join). */
 
 var monthsOneWord = months.join('-');
-console.log(monthsOneWord);
+//console.log(monthsOneWord);
 
 /* 3.g) Crear una copia del array de meses que contenga desde Mayo hasta Noviembre
 (utilizar slice). */
 
 var monthsMayNov = months2.slice(4,11);
-console.log(monthsMayNov);
+//console.log(monthsMayNov);
 
 
+/* 4.a) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es
+mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un
+alerta con el mensaje “Lower than 0,5” */
+
+var r = Math.random();
+
+if (r >= 0.5) {
+    console.log('Greater than 0.5')
+} else {
+    console.log('Lower than 0.5')
+}
+
+/* 4.b) Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los
+siguientes mensajes de alerta:
+i) “Bebe” si la edad es menor a 2 años
+ii) “Nino” si la edad es entre 2 y 12 años
+iii) “Adolecente” si la edad es entre 13 y 19 años
+iv) “Joven” si la edad está entre 20 y 30 años
+v) “Adulto” entre 31 y 60 años
+vi) “Adulto mayor” entre 61 y 75 años
+vii) “Anciano” si es mayor a 75 años */
+
+var age = 1;
+
+if (age<2){
+    console.log('Bebe')
+} else if (age>=2 && age<=12){
+    console.log('Nino')
+} else if (age>12 && age<=19){
+    console.log('Adolescente')
+} else if (age>19 && age<=30){
+    console.log('Joven')
+} else if (age>30 && age<=60){
+    console.log('Adulto')
+} else if (age>60 && age<=75){
+    console.log('Adulto mayor')
+} else if (age>75){
+    console.log('Anciano')
+}
 
 
 
